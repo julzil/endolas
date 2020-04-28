Set up the generator, mask rotation issue to be fixed

lastengen.py
@@ -5,6 +5,7 @@ import os
 import json
 import random
 import keys
+import glob
 
 from tensorflow import keras
 from tensorflow.keras.utils import Sequence
@@ -12,7 +13,7 @@ from tensorflow.keras.utils import Sequence
 
 class LASTENSequence(Sequence):
     def __init__(self, path, batch_size=32, image_ids=None, preprocess_input=None, augment=False, shuffle=False,
-                 width=256, height=512, seed=42):
+                 width=256, height=512, grid_width=18, grid_height=18, seed=42):
         """ Object for fitting to a sequence of data of the BAGLS dataset. Anterior and posterior points are considered
             as labels. In augmentation a rotation is only applied if the first attempt did not rotate a keypoint out of
             the image.
@@ -41,21 +42,22 @@ class LASTENSequence(Sequence):
         random.seed(seed)
 
         self._path = path
-        self._image_ids = image_ids
         self._batch_size = batch_size
         self._preprocess_input = preprocess_input
         self._augment = augment
         self._shuffle = shuffle
         self._width = width
         self._height = height
+        self._grid_width = grid_width
+        self._grid_height = grid_height
 
         self._image_id_2_scaling = dict()
 
         self._augmenter_rot = utils.get_augmenter(rotation=True)
         self._augmenter = utils.get_augmenter(rotation=False)
-        self._size = self._get_size(image_ids)
+        self._image_ids = self._get_image_ids(image_ids)
+        self._size = self._get_size(self._image_ids)
         self._length = self._get_length()
-        self._image_id_2_keypoints = self._get_image_id_2_keypoints()
 
     def __len__(self):
         """ On call of len(...) the length of the sequence is returned, which is an integer value that
@@ -74,23 +76,30 @@ class LASTENSequence(Sequence):
         else:
             index_end = index_start + self._batch_size - 1
 
-        X, y = self._get_batch(index_start, index_end)
+        batch_image_ids = self._image_ids[index_start:index_end+1]
+
+        # DEBUG:
+        #print("Start " + str(index_start))
+        #print("End+1 " + str(index_end+1))
+        #print("Batch " + str(batch_image_ids))
+
+        X, y = self._get_batch(batch_image_ids)
 
         return X, y
 
-    def _get_batch(self, index_start, index_end):
+    def _get_batch(self, batch_image_ids):
         """ Retrieves a batch from .png files in path, where the integer interval of [index_start, index_end] defines
             the elements of the batch. X will be preprocessed according to preprocess_input, y will normalized and then
             shifted by 1 such that it is between [1,2], as mape can not be evaluated for 0.
         """
-        n_data_points = index_end - index_start + 1
+        n_data_points = len(batch_image_ids)
 
         X = np.zeros((n_data_points, self._height, self._width, 1))
         y_1 = np.zeros((n_data_points, self._height, self._width, 1))
-        y_2 = np.zeros((n_data_points, 4))
+        y_2 = np.zeros((n_data_points, self._grid_width * self._grid_height * 2))
 
-        for batch_index, index in enumerate(range(index_start, index_end + 1)):
-            image, mask, keypoints = self._get_image_mask_keypoints(index)
+        for batch_index, image_id in enumerate(batch_image_ids):
+            image, mask, keypoints = self._get_image_mask_keypoints(image_id)
 
             X[batch_index] = image
             y_1[batch_index] = mask
@@ -104,35 +113,42 @@ class LASTENSequence(Sequence):
 
         return X, y
 
-    def _get_image_mask_keypoints(self, index):
+    def _get_image_mask_keypoints(self, image_id):
         """ Retrieves one image with its associated mask and keypoints
         """
-        image_id = self._image_ids[index]
-
+        # IMAGE
         path_image = os.path.join(self._path, "{}.png".format(image_id))
 
         image = keras.preprocessing.image.load_img(path_image, color_mode="grayscale")
 
         scale_factor_x = self._width / image.size[0]
         scale_factor_y = self._height / image.size[1]
-        scaling = np.array([scale_factor_x, scale_factor_y, scale_factor_x, scale_factor_y])
+        scaling = np.array([scale_factor_x, scale_factor_y])
         self._image_id_2_scaling[image_id] = scaling
 
         image = image.resize((self._width, self._height))
         image = keras.preprocessing.image.img_to_array(image)
 
-        path_mask = os.path.join(self._path, "{}_seg.png".format(image_id))
+        # MASK
+        path_mask = os.path.join(self._path, "{}_m.png".format(image_id))
 
         mask = keras.preprocessing.image.load_img(path_mask, color_mode="grayscale")
         mask = mask.resize((self._width, self._height))
         mask = keras.preprocessing.image.img_to_array(mask)
         mask = mask / 255
 
-        keypoints = self._image_id_2_keypoints[image_id] * scaling
-        keypoints = np.array([np.clip(keypoints[0], 0.1, (self._width - 0.1)),
-                              np.clip(keypoints[1], 0.1, (self._height - 0.1)),
-                              np.clip(keypoints[2], 0.1, (self._width - 0.1)),
-                              np.clip(keypoints[3], 0.1, (self._height - 0.1))])
+        # KEYPOINT
+        keypoints = self._get_keypoints(image_id)
+        keypoints_cache = np.zeros(self._grid_width * self._grid_height * 2)
+
+        for index, keypoint in enumerate(keypoints):
+            length = self._width if index % 2 == 0 else self._height
+            scale = scaling[0] if index % 2 == 0 else scaling[1]
+
+            keypoint = np.clip(keypoint * scale, 0.1, (length - 0.1))
+            keypoints_cache[index] = keypoint
+
+        keypoints = keypoints_cache
 
         if self._augment:
             image, mask, keypoints = self._run_augmentation(image, keypoints, mask)
@@ -146,11 +162,29 @@ class LASTENSequence(Sequence):
         mask = np.uint8(mask)
         keypoints = np.float32(keypoints)
 
-        keypoints = [(keypoints[0], keypoints[1]), (keypoints[2], keypoints[3])]
+        # Only rotate occuring keypoints
+        keypoints_cache = []
+        rot_index_2_x_index = dict()
+        rot_index = 0
 
+        for index in range(0, self._grid_height * self._grid_width):
+            x_index = index * 2
+
+            x = keypoints[x_index]
+            y = keypoints[x_index + 1]
+
+            if x > 1 and y > 1:
+                keypoints_cache.append((x, y))
+
+                rot_index_2_x_index[rot_index] = x_index
+                rot_index += 1
+
+        keypoints = keypoints_cache
+
+        # Run rotated augmentation and check if points were rotated out of the image.
         augmentation = self._augmenter_rot(image=image, mask=mask, keypoints=keypoints)
 
-        if len(augmentation["keypoints"]) < 2:
+        if len(augmentation["keypoints"]) < rot_index:
             augmentation = self._augmenter(image=image, mask=mask, keypoints=keypoints)
 
         image = augmentation["image"]
@@ -158,10 +192,14 @@ class LASTENSequence(Sequence):
         mask = augmentation["mask"]
         mask = np.round(mask)
 
-        keypoints = np.array([augmentation["keypoints"][0][0],
-                              augmentation["keypoints"][0][1],
-                              augmentation["keypoints"][1][0],
-                              augmentation["keypoints"][1][1]])
+        # Map back the keypoints.
+        keypoints = np.zeros(self._grid_width * self._grid_height * 2)
+
+        for rot_index, keypoint in enumerate(augmentation["keypoints"]):
+            x_index = rot_index_2_x_index[rot_index]
+
+            keypoints[x_index] = keypoint[0]
+            keypoints[x_index + 1] = keypoint[1]
 
         image = np.float64(image)
         mask = np.float64(mask)
@@ -180,10 +218,21 @@ class LASTENSequence(Sequence):
         else:
             X = self._preprocess_input(X)
 
-        normalization = np.array([self._width, self._height, self._width, self._height])
+        normalization = np.array([[self._width, self._height]])
+        normalization = np.repeat(normalization, self._grid_height * self._grid_width, axis=0).flatten()
         y = np.apply_along_axis(lambda keypoints: (keypoints / normalization) + 1, 1, y)
 
         return X, y
+
+    def _get_image_ids(self, image_ids):
+        """ Get a list of image ids that are supposed to be in the data set.
+        """
+        if not image_ids:
+            globs = glob.glob(self._path + os.sep + "*.json")
+            globs = [int(path.split(os.sep)[-1].split(".")[0]) for path in globs]
+            image_ids = sorted(globs)
+
+        return image_ids
 
     def _get_size(self, image_ids):
         """ Get the size that represents the amount of sample points.
@@ -203,31 +252,22 @@ class LASTENSequence(Sequence):
 
         return length
 
-    def _get_image_id_2_keypoints(self):
+    def _get_keypoints(self, image_id):
         """ Create a dictionary from the 'ap.points' file that maps an image_id to anterior and posterior points.
         """
-        path_labels = os.path.join(self._path, "ap.points")
+        path_labels = os.path.join(self._path, "{}.json".format(image_id))
         file_labels = open(path_labels)
         data_labels = json.load(file_labels)
         file_labels.close()
 
-        existing_image_ids = set()
-        for roi in data_labels["rois"]:
-            existing_image_ids.add(roi["z"])
+        keypoints = np.zeros(self._grid_width * self._grid_height * 2)
 
-        image_id_2_y = dict()
-        for image_id in existing_image_ids:
-            image_id_2_y[image_id] = np.zeros(4)
+        for key, value in data_labels.items():
+            key = int(key)
+            keypoints[(key * 2) + 0] = value[0]
+            keypoints[(key * 2) + 1] = value[1]
 
-        for roi in data_labels["rois"]:
-            image_id = roi["z"]
-            keypoint_id = roi["id"]
-            values = roi["pos"]
-
-            image_id_2_y[image_id][(keypoint_id * 2) + 0] = values[0]
-            image_id_2_y[image_id][(keypoint_id * 2) + 1] = values[1]
-
-        return image_id_2_y
+        return keypoints
 
     def on_epoch_end(self):
         """ Prepare next epoch