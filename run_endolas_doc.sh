de07357406b8448eecc5f33618791f071ad75b20
Julian Zilker <Julian.Zilker@uni-wuerzburg>
2021-03-16 10:13:24 +0100

    Clean repository and updated figure

--- run_endolas_doc.sh
+++
@@ -1,8 +0,0 @@
-sphinx-apidoc -M -o endolas_doc/rsts endolas --ext-todo --force
-rm endolas_doc/rsts/modules.rst
-cd endolas_doc
-make html
-cd ..
-rm endolas_doc.html
-ln -s endolas_doc/_build/html/index.html endolas_doc.html
-
